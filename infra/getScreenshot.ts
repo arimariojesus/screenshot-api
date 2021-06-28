5fd0fa7f712089b95414575db9a3728de59fa4df
Ari Jesus <arimario.jesus@hotmail.com>
2021-06-28 01:36:32 -0300

    fix: platform type on getScreenshot

--- infra/getScreenshot.ts
+++ infra/getScreenshot.ts
@@ -8,9 +8,18 @@ export async function getOptions() {
   const chromeExecPaths = {
     win32: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
     linux: '/usr/bin/google-chrome',
-    darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    // Implicit types
+    aix: '',
+    android: '',
+    freebsd: '',
+    haiku: '',
+    openbsd: '',
+    sunos: '',
+    cygwin: '',
+    netbsd: '',
   };
-  
+
   const exePath = chromeExecPaths[process.platform];
 
   if (isDev) {
